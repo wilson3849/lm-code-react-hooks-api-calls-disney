ab7bced9ab18ca41c6291221b4823e137285395e
wilson3849 <wilson3849@gmail.com>
2022-04-29 21:37:35 +0100

    Activity 1 is passed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/header';
 import CharacterContainer from './components/character_container';
 import Navigation from './components/navigation';
 import { DisneyCharacter } from './disney_character';
+import axios from 'axios';
 
 const App : React.FC = () => {
 
 	const [currentPage, setCurrentPage] = useState<number>(1);
 
+  const [characters, setCharacters] = useState<Array<DisneyCharacter>>([]);
+
   // Some dummy state representing disney characters
+  /*
   const [characters, setCharacters] = useState<Array<DisneyCharacter>>([
     {
       _id: 6,
@@ -28,14 +32,33 @@ const App : React.FC = () => {
       imageUrl: "https://static.wikia.nocookie.net/disney/images/1/1e/Profile_-_Ace.png"
     },
   ]);
+  */
 
-  
+  useEffect(() => {
+    getCharacters(1);
+  }, []);  
+
+  const getCharacters = async (pageNumber : number) => {
+    axios.get(`https://api.disneyapi.dev/characters?page=${pageNumber}`)
+    .then(function (response) {
+        // handle success
+        setCharacters(response.data.data)
+        console.table(characters)
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    });
+  };
 
   return (
+
     <div className="page">
+
       <Header currentPage={currentPage} />
       <Navigation currentPage={currentPage} setCurrentPage={setCurrentPage} />
       <CharacterContainer characters={characters} />
+
     </div>
   );
 }
