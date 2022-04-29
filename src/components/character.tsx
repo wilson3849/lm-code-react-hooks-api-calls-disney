445f671272c3f947edcfaf15e1ff55557ac0ec18
wilson3849 <wilson3849@gmail.com>
2022-04-29 21:45:31 +0100

    Activity 2 change is made.

--- src/components/character.tsx
+++ src/components/character.tsx
@@ -2,8 +2,14 @@ import { DisneyCharacter } from "../disney_character"
 
 // for our props we can reuse the DisneyCharacter interface
 // - defining an anonymous type that just has one property - a DisneyCharacter
-const Character : React.FC<{ character: DisneyCharacter}> = ( { character }) => 
+const Character : React.FC<{ character: DisneyCharacter}> = ( { character }) => { 
   
+  let imageSrc = "https://picsum.photos/300/200/?blur";
+  if (character.imageUrl) {
+    imageSrc = ((character.imageUrl.indexOf('/revision') > -1) ? character.imageUrl.substring(0, character.imageUrl.indexOf('/revision')) : character.imageUrl);
+  }  
+
+  return (
     <article className="character-item">
 
       <h2>{character.name}</h2>
@@ -12,10 +18,11 @@ const Character : React.FC<{ character: DisneyCharacter}> = ( { character }) =>
         Add to Favourites
       </div>
 
-      <img className="character-item__img" src={character.imageUrl} alt={character.name} />
+      <img className="character-item__img" src={imageSrc} alt={character.name} />
 
     </article>
-  
+  )
+}
 
 
 export default Character
